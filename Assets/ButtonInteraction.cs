f339e236128fb3b1461ab45e4a2fde33fc37d839
AntoineMorey <85560122+AntoineMorey@users.noreply.github.com>
2022-01-19 15:40:16 +0100

    a

--- Assets/ButtonInteraction.cs
+++ Assets/ButtonInteraction.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonInteraction : MonoBehaviour
+
 {
+
+    public TextMesh textchange;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public void setTxt()
+    {
+        textchange.text =("Ta mère elle suce des ours");
+    }
+
+
 }
