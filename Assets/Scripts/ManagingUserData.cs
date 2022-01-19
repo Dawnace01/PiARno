4a79959065729518b82757e883f34fb032c94a66
Doriancj <doriancj.gorse@gmail.com>
2022-01-19 16:06:47 +0100

    Reading JSON file to get data

---
+++ Assets/Scripts/ManagingUserData.cs
@@ -0,0 +1,30 @@
+using SimpleJSON;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ManagingUserData : MonoBehaviour
+{
+
+    public string fileName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string toParse = Read();
+        JSONNode parsed = JSON.Parse(toParse);
+
+        this.GetComponent<TextMesh>().text = parsed.GetValueOrDefault("Name", "Guest") + "\nLvl: " + parsed.GetValueOrDefault("Niveau", "None");
+        
+    }
+
+    private string Read()
+    {
+        StreamReader sr = new StreamReader(Application.dataPath + "/UserData/" + fileName + ".json");
+        string content = sr.ReadToEnd();
+        sr.Close();
+
+        return content;
+    }
+}
